a1ac51e04fd0025540631b5e2f774a0fa4e7ddb9
valefaraz <valefaraz@gmail.com>
2019-06-03 22:26:47 -0300

    comit local

--- 58018-valentin-faraz/clase06/cafe.py
+++ 58018-valentin-faraz/clase06/cafe.py
@@ -1,19 +1,148 @@
-class MaquinaCafe ()
-    def __init__ (self):
-        self.cant_cafe = 10
-        self.cant_agua = 10
-        self.cant_azucar = 10
-        self.cant_monedas = 0
-        self.moneda = True
-    def preparar (self):
-        if self.cant_cafe > 0 :
-            self.cafe = self.cafe - 1
-        if self.cant_agua > 0 :
+class pagoCafetera():
 
-        if self.cant_azucar > 0 :
-    def estado (self):
+    def __init__(self):
+        self.moneda = False
+        self.monedaCont = 0
+        self.haciendoCafe = False
+        self.agua = 1000
+        self.cafe = 100
+        self.azucar = 100
+
+    def pago(self, moneda):
+        if moneda == True:
+            if self.agua == 0:
+                self.moneda = False
+                return 'No hay agua'
+            if self.cafe == 0:
+                self.moneda = False
+                return 'No hay cafe'
+            if self.azucar == 0:
+                self.moneda = False
+                return 'No hay azucar'
+            else:
+                self.haciendoCafe = True
+                self.monedaCont += 1
+                return 'Haciendo cafe'
+
+    def terminado(self):
+        if self.haciendoCafe == True:
+            self.haciendoCafe = False
+            return 'Cafe hecho'
+        else:
+            return 'No puedo hacer el cafe' 
+            
 
 
+class cafeteraBasica(pagoCafetera):
+
+    def __init__(self):
+        super().__init__()
+
+
+    def aguaCant(self, aguaML):
+        if self.haciendoCafe == True:
+            if int(aguaML) > self.agua:
+                self.haciendoCafe = False
+                self.agua = 1000
+                self.cafe = 100
+                self.azucar = 100
+                return 'No hay agua suficiente'
+            else:
+                self.agua -= int(aguaML)
+                return 'Con ' + str(aguaML) + 'ml de agua'
+
+    def cafeCant(self, cafeG):
+        if self.haciendoCafe == True:
+            if cafeG > self.cafe:
+                self.agua = 1000
+                self.cafe = 100
+                self.azucar = 100
+                self.haciendoCafe = False
+                return 'No hay cafe suficiente'
+            else:
+                self.cafe -= cafeG
+                return 'Con ' + str(cafeG) + 'g de cafe'
+
+    def azucarCant(self, azucarG):
+        if self.haciendoCafe == True:
+            if azucarG > self.azucar:
+                self.agua = 1000
+                self.cafe = 100
+                self.azucar = 100
+                self.haciendoCafe = False
+                return 'No hay azucar suficiente'
+            if azucarG == 0:
+                return 'Sin azucar'
+            else:
+                self.azucar -= azucarG
+                return 'Con ' + str(azucarG) + 'g de azucar' 
+
+
+class cafeteraPremium(pagoCafetera):
+    def __init__(self):
+        super().__init__()
+
+    
+    def vasoSN(self, vasoOP):
+        if vasoOP == False:
+            self.haciendoCafe = False
+            return 'Ponga un vaso'
+            
+
+    def aguaCantP(self, aguaMLP):
+        if self.haciendoCafe == True:
+            if int(aguaMLP) > self.agua:
+                self.agua = 1000
+                self.cafe = 100
+                self.azucar = 100
+                self.haciendoCafe = False
+                return 'No hay agua suficiente'
+            else:
+                self.agua -= int(aguaMLP)
+                return 'Con ' + str(aguaMLP) + 'ml de agua'
+
+    def cafeCantP(self, cafeGP):
+        if self.haciendoCafe == True:
+            if cafeGP > self.cafe:
+                self.agua = 1000
+                self.cafe = 100
+                self.azucar = 100
+                self.haciendoCafe = False
+                return 'No hay cafe suficiente'
+            else:
+                self.cafe -= cafeGP
+                return 'Con ' + str(cafeGP) + 'g de cafe'
+
+    def azucarCantP(self, azucarGP):
+        if self.haciendoCafe == True:
+            if azucarGP > self.azucar:
+                self.agua = 1000
+                self.cafe = 100
+                self.azucar = 100
+                self.haciendoCafe = False
+                return 'No hay azucar suficiente'
+            if azucarGP == 0:
+                return 'Sin azucar'
+            else:
+                self.azucar -= azucarGP
+                return 'Con ' + str(azucarGP) + 'g de azucar'  
+
+    def lecheCant(self, lecheSN):
+        if self.haciendoCafe == True:
+            if lecheSN == True:
+                return 'Con leche'
+            if lecheSN == False:
+                return 'Sin leche'  
+
+    def hacerCafeP(self):
+        if self.haciendoCafe == True:
+            self.haciendoCafe = False
+            return 'Cafe hecho'
+        else:
+            self.agua = 1000
+            self.cafe = 100
+            self.azucar = 100
+            return 'No puedo hacer el cafe'
 
         
 
--- 58018-valentin-faraz/clase06/test_cafe.py
+++ 58018-valentin-faraz/clase06/test_cafe.py
@@ -1,2 +1,120 @@
 import unittest
-from cafe import MaquinaCafe
+from cafe import (
+    cafeteraBasica,
+    cafeteraPremium
+)
+
+class Cafetera_Basica(unittest.TestCase):
+    def setUp(self):
+        self.cafeB = cafeteraBasica()
+
+    def test_cafe_solo(self):
+        #Ingresa moneda y verifica el estado de los ingredientes
+        self.assertEqual(self.cafeB.pago(True), 'Haciendo cafe')
+
+        #Define la cantidad de agua
+        self.assertEqual(self.cafeB.aguaCant(10), 'Con 10ml de agua')
+
+        #Define la cantidad de cafe
+        self.assertEqual(self.cafeB.cafeCant(5), 'Con 5g de cafe')
+
+        #Define la cantidad de azucar
+        self.assertEqual(self.cafeB.azucarCant(0), 'Sin azucar')
+
+        #Se fija si esta todo correcto y hace el cafe o no
+        self.assertEqual(self.cafeB.terminado(), 'Cafe hecho')
+
+        #La maquina queda en: 
+        self.assertEqual(self.cafeB.agua, 990)
+        self.assertEqual(self.cafeB.cafe, 95)
+        self.assertEqual(self.cafeB.azucar, 100)
+
+    def test_cafe(self):
+        self.assertEqual(self.cafeB.pago(True), 'Haciendo cafe')
+        self.assertEqual(self.cafeB.aguaCant(10), 'Con 10ml de agua')
+        self.assertEqual(self.cafeB.cafeCant(7), 'Con 7g de cafe')
+        self.assertEqual(self.cafeB.azucarCant(3), 'Con 3g de azucar')
+        self.assertEqual(self.cafeB.terminado(), 'Cafe hecho')
+
+        self.assertEqual(self.cafeB.agua, 990)
+        self.assertEqual(self.cafeB.cafe, 93)
+        self.assertEqual(self.cafeB.azucar, 97)
+
+    #Hacemos de cuenta que el producto en la cafetera es menor a la que necesita el vaso
+    def test_cafe_agua_insuficiente(self):
+        self.assertEqual(self.cafeB.pago(True), 'Haciendo cafe')
+        self.assertEqual(self.cafeB.aguaCant(9999), 'No hay agua suficiente')
+        self.assertEqual(self.cafeB.terminado(), 'No puedo hacer el cafe')
+
+        self.assertEqual(self.cafeB.agua, 1000)
+        self.assertEqual(self.cafeB.cafe, 100)
+        self.assertEqual(self.cafeB.azucar, 100)
+        
+
+    def test_cafe_cafe_insuficiente(self):
+        self.assertEqual(self.cafeB.pago(True), 'Haciendo cafe')
+        self.assertEqual(self.cafeB.aguaCant(10), 'Con 10ml de agua')
+        self.assertEqual(self.cafeB.cafeCant(9999), 'No hay cafe suficiente')
+        self.assertEqual(self.cafeB.terminado(), 'No puedo hacer el cafe')
+
+        self.assertEqual(self.cafeB.agua, 1000)
+        self.assertEqual(self.cafeB.cafe, 100)
+        self.assertEqual(self.cafeB.azucar, 100)
+
+    def test_cafe_azucar_insuficiente(self):
+        self.assertEqual(self.cafeB.pago(True), 'Haciendo cafe')
+        self.assertEqual(self.cafeB.aguaCant(10), 'Con 10ml de agua')
+        self.assertEqual(self.cafeB.cafeCant(5), 'Con 5g de cafe')
+        self.assertEqual(self.cafeB.azucarCant(9999), 'No hay azucar suficiente')
+        self.assertEqual(self.cafeB.terminado(), 'No puedo hacer el cafe')
+
+        self.assertEqual(self.cafeB.agua, 1000)
+        self.assertEqual(self.cafeB.cafe, 100)
+        self.assertEqual(self.cafeB.azucar, 100)
+
+
+class Cafetera_Premium(unittest.TestCase):
+    def setUp(self):
+        self.cafeA = cafeteraPremium()
+
+    def test_cafe_solo_con_leche(self):
+        self.assertEqual(self.cafeA.pago(True), 'Haciendo cafe')
+        self.assertEqual(self.cafeA.aguaCantP(10), 'Con 10ml de agua')
+        self.assertEqual(self.cafeA.cafeCantP(5), 'Con 5g de cafe')
+        self.assertEqual(self.cafeA.azucarCantP(0), 'Sin azucar')
+        self.assertEqual(self.cafeA.lecheCant(True), 'Con leche')
+        self.assertEqual(self.cafeA.hacerCafeP(), 'Cafe hecho')
+
+
+        self.assertEqual(self.cafeA.agua, 990)
+        self.assertEqual(self.cafeA.cafe, 95)
+        self.assertEqual(self.cafeA.azucar, 100)
+
+
+    def test_cafe_sin_vaso(self):
+        self.assertEqual(self.cafeA.pago(True), 'Haciendo cafe')
+        self.assertEqual(self.cafeA.vasoSN(False), 'Ponga un vaso')
+        self.assertEqual(self.cafeA.hacerCafeP(), 'No puedo hacer el cafe')
+
+
+        self.assertEqual(self.cafeA.agua, 1000)
+        self.assertEqual(self.cafeA.cafe, 100)
+        self.assertEqual(self.cafeA.azucar, 100)
+
+    def test_cafe_sin_leche(self):
+        self.assertEqual(self.cafeA.pago(True), 'Haciendo cafe')
+        self.assertEqual(self.cafeA.aguaCantP(7), 'Con 7ml de agua')
+        self.assertEqual(self.cafeA.cafeCantP(3), 'Con 3g de cafe')
+        self.assertEqual(self.cafeA.azucarCantP(2), 'Con 2g de azucar')
+        self.assertEqual(self.cafeA.lecheCant(False), 'Sin leche')
+        self.assertEqual(self.cafeA.hacerCafeP(), 'Cafe hecho')
+
+
+        self.assertEqual(self.cafeA.agua, 993)
+        self.assertEqual(self.cafeA.cafe, 97)
+        self.assertEqual(self.cafeA.azucar, 98)
+        
+
+
+if __name__ == "__main__":
+    unittest.main()
